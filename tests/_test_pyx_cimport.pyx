2ae4fd00de0a6dadc23c42158aa8ec293607f58d
nocarryr <matt@nomadic-recording.com>
2019-10-24 14:02:28 -0500

    Add benchmark to test cdef vs cpdef functions

--- tests/_test_pyx_cimport.pyx
+++ tests/_test_pyx_cimport.pyx
@@ -54,63 +54,67 @@ cdef void unpack_color_tuple(color_tuple_t color, double[:] values) nogil except
         values[i] = color[i]
 
 @cython.cdivision(True)
-cdef void check_values(double[:] a, double[:] b) except *:
+cdef void check_values(double[:] a, double[:] b) nogil except *:
     cdef Py_ssize_t size = a.shape[0]
     cdef Py_ssize_t ndigits = 7
     cdef double mult = ndigits / 10**ndigits
     if b.shape[0] != size:
-        assert b.shape[0] == size
+        with gil:
+            assert b.shape[0] == size
 
     cdef Py_ssize_t i
 
     for i in range(size):
         if roundc(a[i] * mult) != roundc(b[i] * mult):
-            raise Exception('{} != {}, diff={}'.format(a[i], b[i], abs(a[i])-abs(b[i])))
+            with gil:
+                raise Exception('{} != {}, diff={}'.format(a[i], b[i], abs(a[i])-abs(b[i])))
 
-def do_test(double[:,:] color_values):
+def do_test_cdef(double[:,:] color_values, double[:,:] result):
     cdef color_hls_t hls
     cdef color_yiq_t yiq
     cdef color_rgb_t rgb
     cdef color_hsv_t hsv
     cdef Py_ssize_t nrows = color_values.shape[0]
-    cdef double[:,:] result = np.empty((nrows,3), dtype=np.double)
+    cdef double[:] color_value
     cdef double[:] value = np.empty(3, dtype=np.double)
     cdef Py_ssize_t i
 
     for i in range(nrows):
-        assign_color_tuple(rgb.array, color_values[i])
+        color_value = color_values[i]
+
+        assign_color_tuple(rgb.array, color_value)
         _rgb_to_hsv(&rgb, &hsv)
         _hsv_to_rgb(&hsv, &rgb)
         unpack_color_tuple(rgb.array, value)
         assert check_hsv_values(&hsv)
         assert check_rgb_values(&rgb)
-        check_values(value, color_values[i])
+        check_values(value, color_value)
 
-        assign_color_tuple(yiq.array, color_values[i])
+        assign_color_tuple(yiq.array, color_value)
         _yiq_to_rgb(&yiq, &rgb)
         _rgb_to_yiq(&rgb, &yiq)
         unpack_color_tuple(yiq.array, value)
         assert check_yiq_values(&yiq)
         assert check_rgb_values(&rgb)
-        check_values(value, color_values[i])
+        check_values(value, color_value)
 
-        assign_color_tuple(hls.array, color_values[i])
+        assign_color_tuple(hls.array, color_value)
         _hls_to_rgb(&hls, &rgb)
         _rgb_to_hls(&rgb, &hls)
         unpack_color_tuple(hls.array, value)
         assert check_hls_values(&hls)
         assert check_rgb_values(&rgb)
-        check_values(value, color_values[i])
+        check_values(value, color_value)
 
-        assign_color_tuple(hsv.array, color_values[i])
+        assign_color_tuple(hsv.array, color_value)
         _hsv_to_rgb(&hsv, &rgb)
         _rgb_to_hsv(&rgb, &hsv)
         unpack_color_tuple(hsv.array, value)
         assert check_hsv_values(&hsv)
         assert check_rgb_values(&rgb)
-        check_values(value, color_values[i])
+        check_values(value, color_value)
 
-        assign_color_tuple(rgb.array, color_values[i])
+        assign_color_tuple(rgb.array, color_value)
         _rgb_to_hsv(&rgb, &hsv)
         _hsv_to_rgb(&hsv, &rgb)
         _rgb_to_hls(&rgb, &hls)
@@ -118,8 +122,49 @@ def do_test(double[:,:] color_values):
         _rgb_to_yiq(&rgb, &yiq)
         _yiq_to_rgb(&yiq, &rgb)
         unpack_color_tuple(rgb.array, value)
-        check_values(value, color_values[i])
+        check_values(value, color_value)
 
         result[i,:] = value
 
     return result
+
+def do_test_cpdef(double[:,:] color_values, double[:,:] result):
+    cdef Py_ssize_t nrows = color_values.shape[0]
+    cdef Py_ssize_t i
+    cdef double[:] color_value
+    cdef double[:] value = np.empty(3, dtype=np.double)
+    cdef double[3] val1, val2
+
+    for i in range(nrows):
+        color_value = color_values[i]
+
+        val1 = rgb_to_hsv(color_value[0], color_value[1], color_value[2])
+        val2 = hsv_to_rgb(val1[0], val1[1], val1[2])
+        value = val2
+        check_values(value, color_value)
+
+        val1 = yiq_to_rgb(color_value[0], color_value[1], color_value[2])
+        val2 = rgb_to_yiq(val1[0], val1[1], val1[2])
+        value = val2
+        check_values(value, color_value)
+
+        val1 = hls_to_rgb(color_value[0], color_value[1], color_value[2])
+        val2 = rgb_to_hls(val1[0], val1[1], val1[2])
+        value = val2
+        check_values(value, color_value)
+
+        val1 = hsv_to_rgb(color_value[0], color_value[1], color_value[2])
+        val2 = rgb_to_hsv(val1[0], val1[1], val1[2])
+        value = val2
+        check_values(value, color_value)
+
+        val1 = rgb_to_hsv(color_value[0], color_value[1], color_value[2])
+        val2 = hsv_to_rgb(val1[0], val1[1], val1[2])
+        val1 = rgb_to_hls(val2[0], val2[1], val2[2])
+        val2 = hls_to_rgb(val1[0], val1[1], val1[2])
+        val1 = rgb_to_yiq(val2[0], val2[1], val2[2])
+        val2 = yiq_to_rgb(val1[0], val1[1], val1[2])
+        value = val2
+        check_values(value, color_value)
+
+        result[i,:] = value
--- tests/test_pyx_cimport.py
+++ tests/test_pyx_cimport.py
@@ -1,7 +1,14 @@
+import pytest
 import numpy as np
 
-from _test_pyx_cimport import do_test
+from _test_pyx_cimport import do_test_cdef, do_test_cpdef
 
-def test_pyx(color_values):
-    result = do_test(color_values)
+@pytest.mark.parametrize('cy_mode', ['cdef', 'cpdef'])
+def test_pyx(benchmark, cy_mode, color_values):
+    nrows = color_values.shape[0]
+    result = np.zeros((nrows,3), dtype=np.double)
+    if cy_mode == 'cdef':
+        benchmark(do_test_cdef, color_values, result)
+    else:
+        benchmark(do_test_cpdef, color_values, result)
     assert np.allclose(color_values, result)
